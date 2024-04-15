32cdad56493d3f60fa465f0f12dd011575e60562
sivatheja <104832308+sivatheja70@users.noreply.github.com>
2024-04-15 13:06:59 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -5,6 +5,7 @@ const buildNumber = process.env.BuildNumber;
 export default ({ config }): ExpoConfig => ({
   ...config,
   name: process.env.APP_NAME,
+  slug: process.env.APP_SLUG,
  android: {
    
     package: "com.smash.in"
